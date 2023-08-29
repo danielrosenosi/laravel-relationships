713173d146a9519be1b3487879711c460d30cc13
danielrosenosi <devroseno@gmail.com>
2023-08-29 13:48:47 -0300

    feat: models finished

---
+++ app/Models/Lesson.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'module_id',
+        'name',
+        'video',
+    ];
+
+    public function module(): BelongsTo
+    {
+        return $this->belongsTo(Module::class);
+    }
+}
