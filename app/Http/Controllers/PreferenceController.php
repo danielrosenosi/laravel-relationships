ecab4ec5476c7500f2b50dd585c9c0820d258666
danielrosenosi <devroseno@gmail.com>
2023-09-09 10:51:02 -0300

    feat: PreferenceController creation

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -2,16 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ShowPreferenceRequest;
+use App\Http\Requests\StorePreferenceRequest;
+use App\Http\Requests\UpdatePreferenceRequest;
 use App\Http\Resources\PreferenceResource;
 use App\Models\Preference;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PreferenceController extends Controller
 {
+    public function show(int $userId): PreferenceResource
+    {
+        $preference = Preference::where('user_id', $userId)->with('user')->get();
+
+        return new PreferenceResource($preference);
+    }
+
     public function index(): ResourceCollection
     {
         $preferences = Preference::with('user')->paginate();
 
         return PreferenceResource::collection($preferences);
+    }
+
+    public function store(StorePreferenceRequest $request): PreferenceResource
+    {
+        $preference = Preference::create($request->validated());
+
+        return new PreferenceResource($preference);
+    }
+
+    public function update(UpdatePreferenceRequest $request, int $userId): PreferenceResource
+    {
+        $preference = Preference::findOrFail($userId);
+
+        $preference->update($request->validated());
+
+        return new PreferenceResource($preference);
     }
 }
