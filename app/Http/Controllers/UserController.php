5f3a6eb2c255d0313ed383fdb30d98e92fcb55af
danielrosenosi <devroseno@gmail.com>
2023-09-09 10:36:00 -0300

    feat: UserController creation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
@@ -15,5 +17,21 @@ class UserController extends Controller
         $users = User::with('preference')->paginate();
 
         return UserResource::collection($users);
+    }
+
+    public function store(StoreUserRequest $request): JsonResponse
+    {
+        User::create($request->validated());
+
+        return response()->json('Usuário criado com sucesso', Response::HTTP_CREATED);
+    }
+
+    public function update(UpdateUserRequest $request, int $id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->update($request->validated());
+
+        return response()->json(new UserResource($user), Response::HTTP_OK);
     }
 }
