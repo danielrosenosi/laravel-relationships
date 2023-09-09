ac964be0e4cd8ffcee78fe0ca1016f93fcf7d208
danielrosenosi <devroseno@gmail.com>
2023-09-09 10:33:17 -0300

    feat: creating update user form request

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O campo é obrigatório',
+            'name.string' => 'O campo deve ser uma string',
+            'email.required' => 'O campo é obrigatório',
+            'email.email' => 'O campo deve ser um email',
+            'password.required' => 'O campo é obrigatório',
+            'password.string' => 'O campo deve ser uma string',
+        ];
+    }
+}
